8c2d9e1b534643a2b614a69af8346492094371aa
woodoo_bti <allance12@mail.ru>
2025-01-22 15:48:43 +0300

    starting adjustment

---
+++ settings.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv("./.env")
+
+
+class Settings(BaseSettings):
+    DATABASE_NAME: str = os.getenv('DATABASE_NAME')
+    DATABASE_HOST: str = os.getenv('DATABASE_HOST')
+    DATABASE_USER: str = os.getenv('DATABASE_USER')
+    DATABASE_PASSWORD: str = os.getenv('DATABASE_PASSWORD')
+    DATABASE_PORT: str = os.getenv('DATABASE_PORT')
+
+    @property
+    def db_path(self):
+        return f"postgresql+asyncpg://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
